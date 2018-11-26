f7277e65daa76e5d5c26ee4cff0ad03547bf2aa7
chrisbcallahan <43003221+chrisbcallahan@users.noreply.github.com>
2018-11-26 12:03:56 -0500

    Add files via upload

--- mysite/people/models.py
+++ mysite/people/models.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class People(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email_address = models.CharField(max_length=250)
+    title = models.CharField(max_length=250)
